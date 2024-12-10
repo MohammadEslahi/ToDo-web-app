c5ba8a22abe1b8155ce042b39571721f0a0f2234
MohammadEslahi <9516email@gmail.com>
2024-12-10 02:09:42 +0000

    Tasks sorting and Priority added

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import *
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('text', 'checked', 'author')
-
+    list_display = ('text', 'author', 'priority', 'checked', 'date_created')
+    readonly_fields = ('date_created',)  # makes 'date_created' read-only (otherwise an error raises)
+    fields = ('text', 'author', 'priority', 'checked', 'date_created')
+    
 
 admin.site.register(Task, TaskAdmin)
--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.homeView , name='home'),
+    path('', views.TasksView , name='home'),
     path('delete/<int:id>', views.deleteView, name='delete'),
     path('update/<int:id>', views.updateView, name='update'),
     path('checker/<int:id>', views.checkerView, name='checker'),
     path('unchecker/<int:id>', views.uncheckerView, name='unchecker'),
-
 ]
