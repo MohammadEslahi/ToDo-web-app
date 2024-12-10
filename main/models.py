0336e098bd8c8b49b7078036459fb8550c3fc00e
MohammadEslahi <9516email@gmail.com>
2024-12-10 02:09:42 +0000

    Tasks sorting and Priority added

--- main/models.py
+++ main/models.py
@@ -6,15 +6,16 @@ from accounts.models import *
 
 class Task(models.Model):
     PRIORITY_CHOICES = [
-        ('low', 'low'),
-        ('medium', 'medium'),
-        ('high','high')
+        ('1-low', 'low'),
+        ('2-medium', 'medium'),
+        ('3-high','high')
     ]
     text = models.CharField(max_length=20)
     checked = models.BooleanField(default=False)
     author = models.ForeignKey (CustomUser, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='taskimages/', blank=True, null=True)
-    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES, default='low')
+    priority = models.CharField(max_length=8, choices=PRIORITY_CHOICES, default='low')
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
