034c8928696eb9e7d168b2280fe39314bbe98022
MohammadEslahi <9516email@gmail.com>
2024-12-10 02:09:42 +0000

    Tasks sorting and Priority added

--- main/views.py
+++ main/views.py
@@ -8,8 +8,9 @@ from django.contrib.auth.decorators import login_required
 
 
 
-def homeView(request):
+def TasksView(request):
     if request.user.is_authenticated:
+        obj = Task.objects.filter(author=request.user).order_by('-priority', '-date_created') # two-factor-sorting
         form = TaskForm(request.POST, request.FILES)
         if request.method == 'POST':
             if form.is_valid():
@@ -17,49 +18,51 @@ def homeView(request):
                 new_instance.author = request.user
                 new_instance.save()
                 return HttpResponseRedirect("/")
+            else:
+                return render(request, 'main.html', {'Task':obj, 'TaskForm':form})
         else:
-            return render(request, 'main.html', {'Task':Task.objects.filter(author=request.user), 'TaskForm':TaskForm})
-    else:
-        return render(request, 'main.html', {})
+            return render(request, 'main.html', {'Task':obj, 'TaskForm':TaskForm})
 
 
 def deleteView(request, id):
-    if request.user==Task.objects.get(id=id).author:
-        obj = get_object_or_404(Task, id = id)
+    obj = get_object_or_404(Task, id=id)
+    if request.user== obj.author:
         if request.method == 'POST':
             obj.delete()
             return HttpResponseRedirect("/")
         
-        return render(request, 'delete.html',{'Task':Task.objects.get(id=id)})
+        return render(request, 'delete.html',{'Task':obj})
     else:
         return HttpResponse("You are not elligible to delete this item")
 
 def updateView(request, id):
-    if request.user==Task.objects.get(id=id).author:
-        obj = Task.objects.get(id=id)
+    obj = get_object_or_404(Task, id=id)
+    form = TaskForm(request.POST, request.FILES)
+    if request.user==obj.author:
         if request.method == 'POST':
-            form = TaskForm(request.POST, request.FILES, instance = obj)
+            form = TaskForm(request.POST, request.FILES, instance=obj)
             if form.is_valid():
                 form.save()
                 return HttpResponseRedirect("/")
+            else:
+                return render(request, 'update.html', {'TaskForm':form(instance=obj), 'obj':obj})
+
         else:
             return render(request, 'update.html', {'TaskForm':TaskForm(instance=obj), 'obj':obj})
-        
-        return render(request, 'update.html', {'obj':obj})
     else:
         return HttpResponse("You are not elligible to edit this item")
 
 
 # for checking tasks
 def checkerView(request, id):
-    obj = Task.objects.get(id=id)
+    obj = get_object_or_404(Task, id=id)
     obj.checked = True
     obj.save()
     return HttpResponseRedirect('/')
 
 
 def uncheckerView(request, id):
-    obj = Task.objects.get(id=id)
+    obj = get_object_or_404(Task, id=id)
     obj.checked = False
     obj.save()
     return HttpResponseRedirect('/')
