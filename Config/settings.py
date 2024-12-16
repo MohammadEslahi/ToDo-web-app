2f5bcb26dc4d8cdad265c6912e1bbb58f02a8257
MohammadEslahi <9516email@gmail.com>
2024-12-16 18:24:39 +0000

    deploy to railway

--- Config/settings.py
+++ Config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['To-Do-app.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
