91a300123cabcad60ae3dc3e415a829c91ef1396
MohammadEslahi <9516email@gmail.com>
2024-12-17 03:45:12 +0000

    Collect static files

--- Config/settings.py
+++ Config/settings.py
@@ -12,8 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # DATABASE CONFIGURATION
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 
@@ -122,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
